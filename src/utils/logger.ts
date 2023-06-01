af7f3ce1f063e17b2e844e13bb98adb43221e0bc
arashroot <haghgoodil.mohammad@gmail.com>
2023-06-01 18:42:50 +0200

    Create logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,24 @@
+import * as R from 'ramda'
+import * as Winston from 'winston'
+
+const { createLogger, format } = Winston
+
+const enumerateErrorFormat = format((info: any) => {
+  R.forEachObjIndexed((value, key) => {
+    if (info[key] instanceof Error) {
+      info[key] = {
+        message: info[key].message,
+        stack: info[key].stack, ...info[key]
+      }
+    }
+  }, info)
+
+  return info
+})
+export const logger = createLogger({
+  format: format.combine(
+    enumerateErrorFormat(),
+    format.json()
+  ),
+  transports: [new Winston.transports.Console()]
+})
